d17490d79b4369c7c8cdb5e024cb0561ddde6851
aodq <dtoadq@gmail.com>
2020-10-14 21:36:28 -0400

    adds exclusive range for raycast X component

--- libraries/physics/src/pulcher-physics/intersections.cpp
+++ libraries/physics/src/pulcher-physics/intersections.cpp
@@ -8,11 +8,11 @@ pulcher::physics::IntersectorRay pulcher::physics::IntersectorRay::Construct(
 ) {
   pulcher::physics::IntersectorRay ray;
   if (beginOrigin.x < endOrigin.x) {
-    ray.beginOrigin.x = glm::round(beginOrigin.x);
-    ray.endOrigin.x = glm::round(endOrigin.x);
+    ray.beginOrigin.x = glm::floor(beginOrigin.x);
+    ray.endOrigin.x = glm::ceil(endOrigin.x);
   } else {
-    ray.beginOrigin.x = glm::round(beginOrigin.x);
-    ray.endOrigin.x = glm::round(endOrigin.x);
+    ray.beginOrigin.x = glm::ceil(beginOrigin.x);
+    ray.endOrigin.x = glm::floor(endOrigin.x);
   }
 
   if (beginOrigin.y < endOrigin.y) {
