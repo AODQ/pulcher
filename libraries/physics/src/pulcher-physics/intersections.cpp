efa13b5752ba5a250303f81c773afb626088549e
aodq <dtoadq@gmail.com>
2020-10-25 19:27:20 -0400

    round intersection ray rather than ceil/floor
      - doesn't seem to have much effect anyway

--- libraries/physics/src/pulcher-physics/intersections.cpp
+++ libraries/physics/src/pulcher-physics/intersections.cpp
@@ -7,27 +7,8 @@ pul::physics::IntersectorRay pul::physics::IntersectorRay::Construct(
   glm::vec2 const beginOrigin, glm::vec2 const endOrigin
 ) {
   pul::physics::IntersectorRay ray;
-  if (glm::round(beginOrigin.x) == glm::round(endOrigin.x)) {
-    ray.beginOrigin.x = glm::round(beginOrigin.x);
-    ray.endOrigin.x = glm::round(endOrigin.x);
-  } else if (beginOrigin.x < endOrigin.x) {
-    ray.beginOrigin.x = glm::floor(beginOrigin.x);
-    ray.endOrigin.x = glm::ceil(endOrigin.x);
-  } else {
-    ray.beginOrigin.x = glm::ceil(beginOrigin.x);
-    ray.endOrigin.x = glm::floor(endOrigin.x);
-  }
-
-  if (glm::round(beginOrigin.y) == glm::round(endOrigin.y)) {
-    ray.beginOrigin.y = glm::round(beginOrigin.y);
-    ray.endOrigin.y = glm::round(endOrigin.y);
-  } else if (beginOrigin.y < endOrigin.y) {
-    ray.beginOrigin.y = glm::floor(beginOrigin.y);
-    ray.endOrigin.y = glm::ceil(endOrigin.y);
-  } else {
-    ray.beginOrigin.y = glm::ceil(beginOrigin.y);
-    ray.endOrigin.y = glm::floor(endOrigin.y);
-  }
+  ray.beginOrigin = glm::round(beginOrigin);
+  ray.endOrigin = glm::round(endOrigin);
 
   return ray;
 }
